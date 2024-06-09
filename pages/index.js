1405e7cb4bffb983691fe57bbcbfa788b1c95c10
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-09 17:11:35 +0300

    feat: Add a homepage

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.unsplash.com/photo-1564511287568-54483b52a35e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1588007789786-3313bc8a0016?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+function Homepage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default Homepage;
